55d21868aef3dc5f452b7d0b3b82fdc7221596f0
Sarvesh Yadav <44526730+alexdeathway@users.noreply.github.com>
2023-08-24 17:49:38 +0530

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -33,5 +33,16 @@ done
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python manage.py migrate
-#python3 manage.py loaddata db.json
+
+if [[ "$DEBUG" == "True" ]]; then
+  if [ -f "db.json" ]; then
+    echo "Loading dummy database..."
+    python3 manage.py loaddata db.json
+  else
+    echo "looks like there is no dummy database or fixture to load..."
+  fi
+else
+  echo "This is not a drill...."
+fi
+
 exec "$@"
