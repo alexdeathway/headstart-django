d10b52d3e9c200e1a04547a82fda6fe51df13e0f
Sarvesh Yadav <44526730+alexdeathway@users.noreply.github.com>
2025-02-12 19:59:09 +0530

    Update manager.sh to monitor muti file operations

--- nginx/manager.sh
+++ nginx/manager.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-echo "Checking for dhparams.pem"
-if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
-  echo "dhparams.pem does not exist - creating it"
-  openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
-fi
+# echo "Checking for dhparams.pem"
+# if [ ! -f "/vol/proxy/ssl-dhparams.pem" ]; then
+#   echo "dhparams.pem does not exist - creating it"
+#   openssl dhparam -out /vol/proxy/ssl-dhparams.pem 2048
+# fi
 
 echo "Checking for fullchain.pem"
 if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
@@ -18,10 +18,12 @@ else
 fi
 
 # Monitor /etc/nginx/ and /etc/letsencrypt/ for configuration or SSL changes
-inotifywait -m -e close_write /etc/nginx/ /etc/letsencrypt/ |
+inotifywait -m -r -e modify,move,close_write /etc/nginx/conf.d/default.conf /etc/letsencrypt |
 while read path action file; do
-    echo "Changes in nginx configuration detected, reloading nginx!"
+    echo "Change detected in $file: $action"
+    echo "Reloading nginx!"
     nginx -s reload
 done &
+
 
 nginx-debug -g 'daemon off;'
